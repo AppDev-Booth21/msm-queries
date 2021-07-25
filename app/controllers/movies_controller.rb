abf0ba65bbcefdbb78c49e7e42f540fc692673b3
jweinheibooth <jweinhei@chicagobooth.edu>
2021-07-25 22:13:28 +0000

    81% done

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+    render( { :template => "movies_templates/index.html.erb"})
+  end
+
+  def movie_details
+    # params looks like {"an_id"=>"42"}
+    
+    the_id = params.fetch("an_id")
+    @the_movie = Movie.where({ :id => the_id}).at(0)
+    #@filmography = Movie.where({ :director_id => @the_director.id })
+
+    render( { :template => "movies_templates/show.html.erb"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,14 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index"})
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
   get("/directors/youngest", { :controller => "directors", :action => "spryest"})
+  
+  get("/movies", { :controller => "movies", :action => "index"})
+  
+  get("/actors", { :controller => "actors", :action => "index"})
+  
+  
+  
   get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details"})
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details"})
 end
